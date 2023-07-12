e601c2265c1167672f06673165475937461165df
fanSam-SAM <139284092+fanSam-SAM@users.noreply.github.com>
2023-07-12 00:51:43 +0000

    post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import './App.css';
-import Headers from './Header/Headers';
-import Users from './Header/Users';
-import Songs from './Header/Songs';
-import SOND from './Second/SOND';
 
 function App() {
   return (
     <>
-     <div className="splitheader fixed top-0 left-0 w-full h-full flex items-start justify-between">
-      <div className="firstheaders w-full h-full max-w-[400px]">
-        <Headers/>
-        <Users/>
-        <Songs/>
-      </div>
-      <div className="sedcondparst w-full h-full">
-        <SOND/>
-      </div>
-     </div>
+      <iframe src="https://medzyadvanced.vercel.app" className="h-full fixed top-0 left-0 w-full"/>
     </>
   );
 }
