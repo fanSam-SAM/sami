a85dd0ab5ae235652c771828bc3653728dea2de6
fanSam-SAM <139284092+fanSam-SAM@users.noreply.github.com>
2023-07-12 00:40:03 +0000

    aidnfod

--- src/Second/Page/Home.jsx
+++ src/Second/Page/Home.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 
 function Home() {
+  const [bg, setbg] = useState(null)
+  const [rl, setrl] = useState(0)
+  useLayoutEffect(() => { 
+    let arr = [
+      'https://fansam-sam-urban-space-goldfish-9797ggjxq97v2xq44-3000.preview.app.github.dev/start.jpg',
+      'https://fansam-sam-urban-space-goldfish-9797ggjxq97v2xq44-3000.preview.app.github.dev/sm.jpg',
+      'https://fansam-sam-urban-space-goldfish-9797ggjxq97v2xq44-3000.preview.app.github.dev/pic.jpg'
+    ]
+    let random = Math.floor(Math.random() * arr.length)
+    setbg(arr[random])
+  }, [rl])
+  
   return (
-    <div>
-      Home Page
-    </div>
-  )
+    <>
+        <div className="hero h-screen bg-top bg-cover" style={{backgroundImage: `url(${bg})`}}>
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Welcome to StellarSite
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+    </>
+  );
 }
 
 export default Home
